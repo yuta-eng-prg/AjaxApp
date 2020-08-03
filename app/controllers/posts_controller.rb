496403a2b2a4cfc42f84b2ab384082062d328e04
yuta-eng-prg <horii6773@gmail.com>
2020-08-04 08:06:32 +0900

    Revert "既読機能のサーバーサイドの記述"
    This reverts commit ddbe9f05e5796699694ccb4627cfcd1354d160fc.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,15 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.order(id: "DESC")
+    @posts = Post.all  # すべてのレコードを@postsに代入
+  end
+
+  def new
+    
   end
 
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index
-  end
-
-  def checked
-    post = Post.find(params[:id])
-    if post.checked then
-      post.update(checked: false)
-    else
-      post.update(checked: true)
-    end
-
-    item = Post.find(params[:id])
-    render json: { post: item }
   end
 
 end
